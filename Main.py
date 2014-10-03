a827e7bfc705e881ecc84dde6140e791e4296ca9
Rui Botão <rui.botao@ruibotao.com>
2014-10-03 16:58:08 +0100

    ADD: Use argparse for options

--- Main.py
+++ Main.py
@@ -6,61 +6,25 @@
 from __future__ import print_function
 
 import sys
+import argparse
 
 
-def Usage():  
-  "Show the command line arguments of the utility"
-  
-  print( 'usage: MainApp [args] [-t] [-o]' )
-  print()
-  print( '\tt   - Change value of option T' )
-  print( '\to   - Change value of option O' )
-
-optionT = 0
-optionO = 0  
-
-def CheckArgs( args ): 
-  "Check the parameters that are passed to the utility"
-  global optionT, optionO
-
-  if len( args ) < 1:
-    Usage()
-    return 1
-
-  for a in args:
-    if a[0] == '-' or a[0] == '/':
-      if a[1] == '?' or a[1] == 'H' or a[1] == 'h':
-        Usage()
-        return 1
-      elif a[1] == 't' or a[1] == 'T':
-        optionT = 1
-      elif a[1] == 'o' or a[1] == 'O':
-        optionO = 1
-      else:
-        Usage()
-        return 1
-
-  return 0
-  
-  
 def main():
   "The main function called when the utility is run"
-  global optionT, optionO
-
-  print( 'Main application  V0.00                                    Rui Botão, Portugal' )
+  print( 'Main application  Vx.rr                                    Rui Botão, Portugal' )
   print()
-
-  args = sys.argv[1:]
-  if CheckArgs( args ) != 0:
-    return 1
-    
-  for a in args:
-    print( a )
-
-  print()
-  print( "OptionT", optionT )
-  print( "OptionO", optionO )
   
+  parser = argparse.ArgumentParser(description="<Put application description here>" )  
+  
+  parser.add_argument( "folder", help="Folder name" )
+  parser.add_argument( "-o", "-option", help="One option", default="No", type=str )
+  
+  args = parser.parse_args()
+  
+  print( "Folder", args.folder )
+  print( "Option", args.o )
+  
+
   return 0
   
   
