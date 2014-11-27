3bf97abfdfc8d031f16799c66f93c9bde7a0b2fc
Rui Botão <rui.botao@ruibotao.com>
2014-11-27 17:26:53 +0000

    ADD: Class-Show inheritance

--- SimpleClass.py
+++ SimpleClass.py
@@ -8,8 +8,9 @@ from __future__ import print_function
 import sys
 
 class Person( object ):
-  def __init__( self, name, age ):
+  def __init__( self, name, sex, age ):
     self.__name = name
+    self.__sex = sex
     self.__age = age
 
   def IsAdult( self ):
@@ -30,6 +31,22 @@ class Person( object ):
     return self.__str__()
 
     
+class Man( Person ):
+  def __init__( self, name, age ):
+    Person.__init__( self, name, 'M', age )
+
+  def IsAdult( self ):
+    # Different way to access the base class
+    if super( Man, self ).Age > 100:
+      # very old people are kids again (just to show usage of super)
+      return False
+      
+    return Person.IsAdult( self )
+    
+  def __str__( self ):
+    return "Man - " + Person.__str__( self )
+
+    
 def main():
   "The main function called when the utility is run."
   
@@ -37,12 +54,18 @@ def main():
   print( 'Very simple class' )
   print()
   
-  p = Person( "John", 21 )
+  p = Person( "John", 'M', 21 )
   print( "Is John an adult?", p.IsAdult() )
   p.Age = 4
   print( "Change the age:", p )
   print( "Is John an adult?", p.IsAdult() )
 
+  print()
+  
+  p = Man( "John", 156 )
+  print( "p =", p )
+  print( "Is John an adult?", p.IsAdult() )
+  
   return 0
   
   
